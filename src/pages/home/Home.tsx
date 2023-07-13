17af97929ccea0c2bd9ac5c3d816bc93a2c60d01
Alek30k <alemank23k@gmail.com>
2023-07-13 16:22:52 -0300

    last commit

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -25,14 +25,19 @@ const Home = () => {
       <div className="box box3">
         <ChartBox {...chartBoxProduct} />
       </div>
-      <div className="box box4">{/* <PieChartBox /> */}</div>
+      <div className="box box4">
+        <PieChartBox />
+      </div>
       <div className="box box5">
         <ChartBox {...chartBoxConversion} />
       </div>
       <div className="box box6">
         <ChartBox {...chartBoxRevenue} />{" "}
       </div>
-      <div className="box box7">{/* <BigChartBox /> */}</div>
+      <div className="box box7">
+        {" "}
+        <BigChartBox />{" "}
+      </div>
       <div className="box box8">
         <BarChartBox {...barChartBoxVisit} />
       </div>
