8f10a71710f2d6e0d65bf2a4e4590f2493b55979
Alek30k <alemank23k@gmail.com>
2023-07-13 15:40:16 -0300

    last commit

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -17,16 +17,21 @@ const Home = () => {
   return (
     <div className="home">
       <div className="box box1">
-        {" "}
         <TopBox />
       </div>
-      <div className="box box2">{/* <ChartBox {...chartBoxUser} /> */}</div>
-      <div className="box box3">{/* <ChartBox {...chartBoxProduct} /> */}</div>
+      <div className="box box2">
+        <ChartBox {...chartBoxUser} />
+      </div>
+      <div className="box box3">
+        <ChartBox {...chartBoxProduct} />
+      </div>
       <div className="box box4">{/* <PieChartBox /> */}</div>
       <div className="box box5">
-        {/* <ChartBox {...chartBoxConversion} /> */}
+        <ChartBox {...chartBoxConversion} />
       </div>
-      <div className="box box6">{/* <ChartBox {...chartBoxRevenue} /> */}</div>
+      <div className="box box6">
+        <ChartBox {...chartBoxRevenue} />{" "}
+      </div>
       <div className="box box7">{/* <BigChartBox /> */}</div>
       <div className="box box8">
         {/* <BarChartBox {...barChartBoxVisit} /> */}
