1dad094ff44cb3287a3bd832c95534226ee27047
Alek30k <alemank23k@gmail.com>
2023-07-14 15:13:30 -0300

    last commit

--- src/App.tsx
+++ src/App.tsx
@@ -11,6 +11,8 @@ import User from "./pages/user/User";
 import Product from "./pages/product/Product";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
+const queryClient = new QueryClient();
+
 function App() {
   const Layout = () => {
     return (
