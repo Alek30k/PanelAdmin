19f261d8696831e3270c409747f79eac252a095c
Alek30k <alemank23k@gmail.com>
2023-07-14 10:37:55 -0300

    last commit

--- src/App.tsx
+++ src/App.tsx
@@ -7,6 +7,9 @@ import Footer from "./components/footer/Footer";
 import Menu from "./components/menu/Menu";
 import Login from "./pages/login/Login";
 import "./styles/global.scss";
+import User from "./pages/user/User";
+import Product from "./pages/product/Product";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
 function App() {
   const Layout = () => {
@@ -18,9 +21,9 @@ function App() {
             <Menu />
           </div>
           <div className="contentContainer">
-            {/* <QueryClientProvider client={queryClient}> */}
-            <Outlet />
-            {/* </QueryClientProvider> */}
+            <QueryClientProvider client={queryClient}>
+              <Outlet />
+            </QueryClientProvider>
           </div>
         </div>
         <Footer />
@@ -44,6 +47,14 @@ function App() {
         {
           path: "products",
           element: <Products />,
+        },
+        {
+          path: "/users/:id",
+          element: <User />,
+        },
+        {
+          path: "/products/:id",
+          element: <Product />,
         },
       ],
     },
